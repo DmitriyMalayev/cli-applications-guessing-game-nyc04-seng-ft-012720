c7314e8b9063b76de9d07f30554504c44a91d807
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-26 03:16:27 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 # Code your solution here!
 random_number = rand(6) + 1
-def run_guessing_game(random_number)
+def run_guessing_game
 puts "Enter guess"
   guess = gets.chomp()
   if guess =! random_number
